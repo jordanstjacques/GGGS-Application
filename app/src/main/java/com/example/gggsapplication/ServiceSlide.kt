ee240ea716f6aefe985ad5fac950f7cfe39b8cf3
ViktarHiryn <viktarhiryn@gmail.com>
2022-11-08 15:09:40 -0800

    All dependencies for kotlin firebase

---
+++ app/src/main/java/com/example/gggsapplication/ServiceSlide.kt
@@ -0,0 +1,3 @@
+package com.example.gggsapplication
+
+data class ServiceSlide(var serviceName : String, var description : String)
---
+++ app/src/main/java/com/example/gggsapplication/ServicesSlidesAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.gggsapplication
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+// Creating an adapter for the recycler view on the homepage
+class ServicesSlidesAdapter (private val slideList : ArrayList<ServiceSlide>) :
+    RecyclerView.Adapter<ServicesSlidesAdapter.MyViewHolder>() {
+    class MyViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        val slideTitle : TextView = itemView.findViewById(R.id.ServiceNametv)
+        val slideDescription : TextView = itemView.findViewById(R.id.InfoAboutServicetv)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.service_item,
+            parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentItem = slideList[position]
+        holder.slideTitle.text = currentItem.serviceName
+        holder.slideDescription.text = currentItem.description
+    }
+
+    override fun getItemCount(): Int {
+        return slideList.size
+    }
+}
