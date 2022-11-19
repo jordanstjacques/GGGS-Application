136e801b848e7e4c6094554c7247bfe022b126a2
ViktarHiryn <viktarhiryn@gmail.com>
2022-11-18 19:20:25 -0800

    Allowing the user to include an image of their house (the recycler view will be work in progress for now)

--- app/src/main/java/com/example/gggsapplication/RentALawn/ListALawnActivity.kt
+++ app/src/main/java/com/example/gggsapplication/RentALawn/ListALawnActivity.kt
@@ -1,23 +1,41 @@
 package com.example.gggsapplication.RentALawn
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.gggsapplication.FeaturedHouses.FeaturedHouse
 import com.example.gggsapplication.R
 import com.example.gggsapplication.databinding.ActivityListAlawnBinding
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 
 class ListALawnActivity : AppCompatActivity() {
     private lateinit var binding : ActivityListAlawnBinding
     private lateinit var database: DatabaseReference
+    val ImageBack = 1
+    lateinit var storage : StorageReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityListAlawnBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        storage = FirebaseStorage.getInstance().reference.child("ImageFolder")
 
+        // -------------------------- Managing the Image upload button ------------------------
+        binding.uploadLawnImage.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            startActivityForResult(intent, ImageBack)
+        }
+
+        // -------------------------- Managing the Submit button ---------------------------------
         binding.SubmitLawnInfoBtn.setOnClickListener {
             // Getting all of the values
             val Address = binding.NewLawnAddress.text.toString()
@@ -33,6 +51,27 @@ class ListALawnActivity : AppCompatActivity() {
                 Toast.makeText(this, "Lawn Listed Successfully!!!", Toast.LENGTH_SHORT).show()
             }.addOnFailureListener{
                 Toast.makeText(this, "Failed To List The Lawn", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == ImageBack) {
+            if(resultCode == RESULT_OK){
+                val imageData = data!!.getData()
+                val imageName: StorageReference = storage.child("image" + imageData!!.getLastPathSegment())
+
+                imageName.putFile(imageData).addOnSuccessListener(OnSuccessListener {
+                    imageName.getDownloadUrl().addOnSuccessListener ( OnSuccessListener<Uri> { uri ->
+                        val dataBaseReference: DatabaseReference = FirebaseDatabase.getInstance()
+                            .getReferenceFromUrl("https://gggsapplication-default-rtdb.firebaseio.com/").child("Image")
+                        val hashMap:HashMap<String,String> = HashMap()
+                        hashMap.put("imageUrl", uri.toString())
+                        dataBaseReference.setValue(hashMap)
+                        Toast.makeText(this, "Data was incerted",Toast.LENGTH_SHORT).show()
+                    })
+                })
             }
         }
     }
