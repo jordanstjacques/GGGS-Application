510ffd2e692f9bfaf44834b24dfc1bf0ae088472
ViktarHiryn <viktarhiryn@gmail.com>
2022-12-02 17:43:15 -0800

    I don't know what is going on

--- app/src/main/java/com/example/gggsapplication/FeaturedHouses/AddingFeaturedHouse.kt
+++ app/src/main/java/com/example/gggsapplication/FeaturedHouses/AddingFeaturedHouse.kt
@@ -12,7 +12,6 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.ktx.storage
 
 class AddingFeaturedHouse : AppCompatActivity() {
     private lateinit var binding: ActivityAddingFeaturedHouseBinding
